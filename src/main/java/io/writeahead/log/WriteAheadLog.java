4b2489d18f7e229c7d64ea297e1f545d236b364b
Sai Shashaank V V <archdemon.developer@gmail.com>
2026-07-18 00:25:07 +0530

    completed wal implementation

---
+++ src/main/java/io/writeahead/log/WriteAheadLog.java
@@ -0,0 +1,56 @@
+package io.writeahead.log;
+
+import io.writeahead.log.fileio.FileUtils;
+import io.writeahead.log.models.LogEntry;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteAheadLog {
+
+    private final int batchSize;
+    private final List<LogEntry> batch;
+    private final FileUtils fileUtils;
+
+    public WriteAheadLog(int batchSize, String logPath) throws IOException {
+        this.batchSize = batchSize;
+        this.batch = new ArrayList<>();
+        this.fileUtils = new FileUtils(logPath);
+    }
+
+    public void append(LogEntry entry) throws IOException {
+        batch.add(entry);
+        if(batch.size() == batchSize) {
+            fileUtils.writeAll(batch);
+            batch.clear();
+        }
+    }
+
+    public List<LogEntry> read() throws IOException {
+        return batch;
+    }
+
+    public List<LogEntry> readAll() throws IOException {
+        List<LogEntry> entries = new ArrayList<>();
+        entries.addAll(fileUtils.readAll());
+        entries.addAll(batch);
+        return entries;
+    }
+
+    public List<LogEntry> readFromDisk() throws IOException {
+        return fileUtils.readAll();
+    }
+
+    public List<LogEntry> readBuffer() {
+        return batch;
+    }
+
+    public void close() throws IOException {
+        if(!batch.isEmpty()) {
+            fileUtils.writeAll(batch);
+            batch.clear();
+        }
+        fileUtils.close();
+    }
+}
---
+++ src/main/java/io/writeahead/log/fileio/FileUtils.java
@@ -0,0 +1,59 @@
+package io.writeahead.log.fileio;
+
+import io.writeahead.log.models.LogEntry;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+
+    private final File file;
+    private final FileOutputStream fileOutputStream;
+    private final DataOutputStream dataOutputStream;
+
+    public FileUtils(String filePath) throws IOException {
+        this.file = new File(filePath);
+        this.fileOutputStream = new FileOutputStream(file, true);
+        this.dataOutputStream = new DataOutputStream(fileOutputStream);
+    }
+
+    public void writeSingle(LogEntry logEntry) throws IOException {
+        dataOutputStream.writeInt(logEntry.getSize());
+        dataOutputStream.write(logEntry.getData());
+        dataOutputStream.flush();
+        fileOutputStream.getFD().sync();
+    }
+
+    public void writeAll(List<LogEntry> logEntries) throws IOException {
+        for(LogEntry logEntry : logEntries) {
+            dataOutputStream.writeInt(logEntry.getSize());
+            dataOutputStream.write(logEntry.getData());
+        }
+        dataOutputStream.flush();
+        fileOutputStream.getFD().sync();
+    }
+
+    public List<LogEntry> readAll() throws IOException {
+        List<LogEntry> fileRead = new ArrayList<>();
+        try(DataInputStream dataInputStream = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(file)))) {
+            while (true) {
+                try {
+                    int size = dataInputStream.readInt();
+                    byte[] data = new byte[size];
+                    dataInputStream.readFully(data);
+                    fileRead.add(new LogEntry(size, data));
+                } catch (EOFException ex) {
+                    break;
+                }
+            }
+        }
+        return fileRead;
+    }
+
+    public void close() throws IOException {
+        dataOutputStream.close();
+    }
+}
